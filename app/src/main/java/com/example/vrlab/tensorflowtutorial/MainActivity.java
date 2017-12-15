a9aa33a1491c415f0f287bd12f5a5c2d4cc4ced4
Chitra Parsaila <chitra@paracosma.com>
2017-12-15 11:52:44 +0545

    tensorflow import with aar

--- app/src/main/java/com/example/vrlab/tensorflowtutorial/MainActivity.java
+++ app/src/main/java/com/example/vrlab/tensorflowtutorial/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.vrlab.tensorflowtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 public class MainActivity extends AppCompatActivity {
-
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int test=1;
     }
 }
